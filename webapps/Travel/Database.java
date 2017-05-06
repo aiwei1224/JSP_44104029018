2d5a6da86cd22be005e71874be77b7e5980317c2
aiwei1224 <aiwei_96@live.com>
2017-05-06 08:38:05 +0800

    update

--- webapps/Travel/Database.java
+++ webapps/Travel/Database.java
@@ -2,8 +2,8 @@ package com.database;
 import java.sql.*;
 public class Database{
   private Connection con = null;
-  private  ResultSet rs = null;
   private Statement stmt = null;
+  private ResultSet rs = null;
   private String ip = "";
   private String port = "";
   private String url = "";
@@ -15,29 +15,57 @@ public class Database{
   public Database(){
 
   }
-  public void ConnectDB(){
+  public void connectDB(){
     try{
       url = "jdbc:mysql://" + ip + ":" + port + "/" + db + "?useUnicode=true&characterEncoding=utf-8";
       Class.forName(driver);
-      con = DriverManager.getConnection(url ,user , password);
+      con = DriverManager.getConnection(url,user,password);
       stmt = con.createStatement();
     }catch(Exception ex){
       System.out.println(ex);
     }
-
   }
   public void query(String sql){
     try{
-       rs = stmt.executeQuery(sql);
+      rs = stmt.executeQuery(sql);
+    }catch(Exception ex){
+      System.out.println(ex);
+    }
+  }
+  public void insertData(String name , String location , String photo , String description){
+    try{
+      String sql = "insert into travel_travel(name,location,photo,description) value(?,?,?,?)";
+      PreparedStatement ps = con.prepareStatement(sql);
+      ps.setString(1,name);
+      ps.setString(2,location);
+      ps.setString(3,photo);
+      ps.setString(4,description);
+      int a = ps.executeUpdate();
+    }catch(Exception ex){
+      System.out.println(ex);
+    }
+  }
+  public void editData(String id , String name , String location , String photo , String description){
+    try{
+      String sql = "update travel_travel set name ='"+name+"',location ='"+location+"',photo ='"+photo+"',description ='"+description+"' where id =" +id;
+      int a = stmt.executeUpdate(sql);
+    }catch(Exception ex){
+      System.out.println(ex);
+    }
+  }
+  public void deleteData(String id){
+    try{
+      String sql = "delete from travel_travel where id = " + id + ";";
+      int a = stmt.executeUpdate(sql);
     }catch(Exception ex){
       System.out.println(ex);
     }
   }
   public Connection getCon(){
-      return con;
+    return con;
   }
   public ResultSet getRS(){
-      return rs;
+    return rs;
   }
   public void setIp(String ip){
     this.ip = ip;
@@ -57,7 +85,7 @@ public class Database{
   public void setPassword(String password){
     this.password = password;
   }
-  public void setDrover(String driver){
+  public void setDriver(String driver){
     this.driver = driver;
   }
 }
